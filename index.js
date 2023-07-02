6d1e620c8b2b0bb21393c4f55fe8d35b53cfd64c
duan spot <brandennjonge@gmail.com>
2023-07-02 03:36:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+var customerName="bob";
+var bestCustomer;
+customerName;
+function upperCaseCustomerName(){
+
+    customerName= customerName.toUpperCase();
+    return customerName;
+
+}
+upperCaseCustomerName();
+
+function setBestCustomer(){
+
+bestCustomer='not bob';
+
+}
+function overwriteBestCustomer(){
+
+ bestCustomer="maybe bob";
+
+}
+function leastFavoriteCustomer(){
+
+
+
+}
+function changeLeastFavoriteCustomer(){
+
+
+
+}
+function changeLeastFavoriteCustomer(){
+
+
+
+}
